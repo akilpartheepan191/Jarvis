ee57bbeeeae1303899564a75be51eeaf8fa214da
akil191 <81765475+akilpartheepan191@users.noreply.github.com>
2021-08-11 04:43:32 +0000

    a

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+import keyboard as k
+def wait():
+  time.sleep(0.8)
+
+
+def wai(times):
+  time.sleep(times)
+
+
+def open(app_name):
+  k.send('windows')
+  wait()
+  k.write(app_name)
+  wait()
+  k.send('enter')
+
+
+def close()
+  k.send('ctrl+w')
+
+
+def switch():
+  k.send('alt+tab')
+
+
+def login():
+  open('chrome')
+  wait()
+  k.send('f6')
+  wait()
+  k.write('camu.in')
+  wai(2)
+  k.send('enter')
